d825b380dfbf0052418b0ed28046f6c40a2e3c76
cioarecalexandru <cioarec.alexandru96@yahoo.com>
2018-05-24 23:53:02 +0300

    R-1 Add configuration file

--- RESTaurant/RESTaurant/Startup.cs
+++ RESTaurant/RESTaurant/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RESTaurant
@@ -18,7 +19,7 @@ namespace RESTaurant
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +28,7 @@ namespace RESTaurant
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(configuration["Salutare"]);
             });
         }
     }
