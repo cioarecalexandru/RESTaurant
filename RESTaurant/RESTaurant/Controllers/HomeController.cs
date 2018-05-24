95765f90b6fd850e7f4c70d0253ea08688c63100
cioarecalexandru <cioarec.alexandru96@yahoo.com>
2018-05-25 02:09:16 +0300

    Models and ViewModels

--- RESTaurant/RESTaurant/Controllers/HomeController.cs
+++ RESTaurant/RESTaurant/Controllers/HomeController.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using RESTaurant.Interfaces.Greeting;
 using RESTaurant.Models;
 using RESTaurant.Services.Interfaces.DataAccess;
+using RESTaurant.ViewModels;
 
 namespace RESTaurant.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IGreeting _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeting greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var restaurants = _restaurantData.GetAll();
-            return View(restaurants);
+            var modelView = new HomeIndexViewModel()
+            {
+                MessageOfDay = _greeter.GetMessageOfTheDay(),
+                Restaurants = _restaurantData.GetAll()
+            };
+            return View(modelView);
         }
     }
 }
---
+++ RESTaurant/RESTaurant/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTaurant.Models;
+
+namespace RESTaurant.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public string MessageOfDay { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+    }
+}
